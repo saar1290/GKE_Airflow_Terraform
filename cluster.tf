e14338ec338132bc186a0a91aa3678a9afe20f43
saarb <saarb@boston.local>
2021-11-09 03:35:39 +0200

    Update cluster

--- cluster.tf
+++ cluster.tf
@@ -3,7 +3,7 @@ data "google_client_config" "current" {}
 resource "google_container_cluster" "primary" {
     name        = var.cluster_name
     project     = var.project_id
-    location      = var.region
+    location    = var.region
     description = "Demo GKE Cluster" 
     initial_node_count       = 1
 }
